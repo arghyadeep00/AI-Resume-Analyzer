2418a88d966fa5681f3909904061279845d8f005
arghyadeep00 <arghyadeepshee00@gmail.com>
2026-09-22 14:52:34 +0530

    fix: append '/api' to BASE_URL for production environment

--- Frontend/services/api.js
+++ Frontend/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? process.env.BASE_URL
+      ? process.env.BASE_URL + "/api"
       : "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
