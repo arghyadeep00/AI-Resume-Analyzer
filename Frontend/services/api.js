fd3efaa53a3d8b2ba6bb6a471a4a5917d929dda6
arghyadeep00 <arghyadeepshee00@gmail.com>
2026-09-22 20:45:48 +0530

    feat: set up Docker configuration for frontend and backend services

--- Frontend/services/api.js
+++ Frontend/services/api.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_API_URL + "/api"
-      : "http://localhost:5000/api",
+    (process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000") + "/api",
   headers: {
     "Content-Type": "application/json",
   },
